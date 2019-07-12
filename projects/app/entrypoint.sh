a16adcbcc1f49dd7e19fbb416051b7d8a62f7bfa
Eldar <eldar.cejvanovic@gmail.com>
2019-07-12 12:27:02 +0200

    Updated App entrypoint script.

--- projects/app/entrypoint.sh
+++ projects/app/entrypoint.sh
@@ -30,7 +30,7 @@ ROUTER_BASE_URL=$(echo "${ROUTER_BASE_URL:-/}" | sed 's#/#\\/#g')
 
 # Replace variables
 cat /directus/config.js | \
-sed -e 's#      \"https://demo-api.directus.app/_/\": \"Directus Demo API\"#'$VALUES'#g' | \
+sed -e 's#      \"../_/\": \"Directus Demo API\"#'$VALUES'#g' | \
 sed -e 's/allowOtherAPI: false/allowOtherAPI: '${ALLOW_OTHER_API:-false}'/g' | \
 sed -e 's/routerBaseUrl: "\/"/routerBaseUrl: "'${ROUTER_BASE_URL}'"/g' | \
 sed -e 's/routerMode: "hash"/routerMode: "'${ROUTER_MODE:-hash}'"/g' > /usr/share/nginx/html/config.js
